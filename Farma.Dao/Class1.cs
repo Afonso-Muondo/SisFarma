c58103f0f87179ba2a1ab5f4a6747fe6606407e3
Afonso Muondo <afonso.muondo@gmail.com>
2022-05-18 15:31:09 -0300

    Add project files.

---
+++ Farma.Dao/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Farma.Dao
+{
+    public class Class1
+    {
+    }
+}
---
+++ Farma.Servico/EventosServico.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Farma.Servico
+{
+    public class EventosServico
+    {
+    }
+}
