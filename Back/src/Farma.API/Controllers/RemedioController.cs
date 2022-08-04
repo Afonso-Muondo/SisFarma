8aa3763f1fb235fa690428e5553d5d80af71033c
Afonso Muondo <afonso.muondo@gmail.com>
2022-08-03 21:20:22 -0300

    Seção 2: .NET Core 5

---
+++ Back/src/Farma.API/Controllers/RemedioController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Farma.API.models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Farma.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RemedioController : ControllerBase
+    {
+        public IEnumerable<Remedio> _remedio = new Remedio[] {
+            new Remedio() {
+                RemedioCodigo = 1,
+                Nome = "Paracetamol",
+                Preço = 14.90,
+                Volume = " 500 gramas"
+                },
+                new Remedio() {
+                RemedioCodigo = 2,
+                Nome = "Drenison",
+                Preço = 9.60,
+                Volume = " 10 gramas"
+                }
+        };
+
+        public RemedioController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Remedio> Get()
+        {
+            return _remedio;              
+        }
+
+        [HttpGet("{codigo}")]
+        public IEnumerable<Remedio> Get(int codigo)
+        {
+            return _remedio.Where(remedio => remedio.RemedioCodigo == codigo);              
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post";
+        }
+
+        [HttpPut("{codigo}")]
+        public string Put(int codigo)
+        {
+            return $"Exemplo de Put com id = {codigo}";
+        }
+
+        [HttpDelete("{codigo}")]
+        public string Delete(int codigo)
+        {
+            return $"Exemplo de Delete com codigo = {codigo}";
+        }
+    }
+}
---
+++ Back/src/Farma.API/models/Remedio.cs
@@ -0,0 +1,12 @@
+namespace Farma.API.models
+{
+    public class Remedio
+    {
+        public int RemedioCodigo { get; set; }
+        public string Nome { get; set; }
+        public double Preço { get; set; }
+        public string Volume { get; set; }
+        
+        // public int Quantidade { get; set; }
+    }
+}
